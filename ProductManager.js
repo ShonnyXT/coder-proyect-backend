717439075c1cdada036089013932e94e8e5a996a
Jonatan Eduardo Mamani <jonny.m722@gmail.com>
2023-08-04 23:23:06 -0300

    agregado metodos update y delete

--- ProductManager.js
+++ ProductManager.js
@@ -4,11 +4,11 @@ class ProductManager{
     }
 
     addProduct(product) {
+        // Comprobacion de adicion al carro
         const prod = this.products.find(prod => prod.code === product.code)
-
-        if(prod) {
+        if (!product.id || prod) {
             console.log("")
-            console.log("> Producto Repetido <")
+            console.log("> Campo requerido o Code Repetido<")
         } else {
             this.products.push(product)
         }
@@ -20,20 +20,47 @@ class ProductManager{
 
     getProductById(id) {
         const prod = this.products.find(prod => prod.id === id)
-
         if(prod) {
             console.log(prod)
         } else {
             console.log("> Not found <")
         }
     }
+
+    updateProduct(id, title, description, price, thumbnail, code, stock) {
+        const prod = this.products.find(prod => prod.id === id)
+        const index = this.products.findIndex(prod => prod.id === id)
+
+        if (prod) {
+            this.products[index].title = title
+            this.products[index].description = description
+            this.products[index].price = price
+            this.products[index].thumbnail = thumbnail
+            this.products[index].code = code
+            this.products[index].stock = stock
+            console.log("Se cambio el Producto con id: " + id)
+        }  else {
+            console.log("> Producto no encontrado <")
+        }
+    } 
+
+    deleteProduct(id) {
+        const prod = this.products.find(prod => prod.id === id)
+        const index = this.products.findIndex(prod => prod.id === id)
+        if (prod) {
+            this.products.splice(index,1)
+            console.log("Se elimino el Producto con id: " + id)
+        } else {
+            console.log("> Producto no encontrado <")
+        }
+    }
 }
 
 class Product {
     constructor(title, description, price, thumbnail, code, stock) {
-        // Comprobacion de datos
+        // Comprobacion de campos
         if (!title || !description || !price || !thumbnail || !code || !stock) {
-            return console.log("> Campo requerido <")
+            return
         }
 
         this.title = title
@@ -57,7 +84,7 @@ class Product {
 }
 
 // Productos
-const producto1 = new Product("Arroz", "Dos hermanos", 300, [], "AA123", 20)
+const producto1 = new Product("Fiambres", "Dos hermanos", 300, [], "AA123", 20)
 const producto2 = new Product("Fideos", "Marolio", 250, [], "BB123", 20)
 const producto3 = new Product("Prueba", "Prueba", 999, [], "ZZ999")  // test campo faltante
 const producto4 = new Product("Azucar", "Ledesma", 250, [], "DD123", 20)
@@ -65,7 +92,6 @@ const producto5 = new Product("Prueba", "Prueba", 999, [], "DD123", 20) // test 
 
 // Agregando Productos al carrito
 const productManager = new ProductManager()
-
 productManager.addProduct(producto1)
 productManager.addProduct(producto2)
 productManager.addProduct(producto3)
@@ -80,5 +106,20 @@ productManager.getProducts()
 // Buscar Producto por ID
 console.log("")
 console.log(">> Productos con ID <<")
-productManager.getProductById(3)
+productManager.getProductById(2)
 productManager.getProductById(4) // test buscardor
+
+// Cambiando Producto       (id, title, description, price, thumbnail, code, stock)
+console.log("")
+console.log(">> Productos Cambiados <<")
+productManager.updateProduct(1, "Producto", "Cambiado", 99, [], "ZZ99", 99)
+
+// Borrar un Producto
+console.log("")
+console.log(">> Productos Eliminados <<")
+productManager.deleteProduct(3)
+
+console.log("")
+console.log("")
+console.log(">>> Resultado Final <<<")
+productManager.getProducts()
